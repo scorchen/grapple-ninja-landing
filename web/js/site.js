c3f91f19c80300adb0263a4235959f9f48ffe3e3
Justin Eyre <scorchen@gmail.com>
2018-04-22 22:49:05 -0600

    Adding initial files
    Based on latest boostrap 4 examples

---
+++ web/js/site.js
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>grapple.ninja - Digital Brazilian Jiu Jitsu Journal [TEST]</title>
+    <meta name="description" />
+    <meta name="keywords" content="BJJ Training Journal, Digital, Training Log, BJJ Journal, Journal, Jiu Jitsu Journal, Mat Time, Brazilian Jiu Jitsu" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<link href="/gpninja/content/site.css?20170809081051" rel="stylesheet" type="text/css" />
+
+    <link href="/gpninja/content/cover.css?20180413092504" rel="stylesheet" type="text/css" />
+
+<link rel="apple-touch-icon" sizes="180x180" href="/gpninja/images/apple-touch-icon.png?20180115053741">
+<link rel="icon" type="image/png" sizes="32x32" href="/gpninja/images/favicon-32x32.png?20180115053742">
+<link rel="icon" type="image/png" sizes="16x16" href="/gpninja/images/favicon-16x16.png?20180115053741">
+
+<link rel="mask-icon" href="/gpninja/images/safari-pinned-tab.svg?20180115053742" color="#5bbad5">
+<meta name="apple-mobile-web-app-title" content="grapple.ninja">
+<meta name="application-name" content="grapple.ninja">
+<meta name="theme-color" content="#000000">
+
+
+
+    
+    
+</head>
+<body class="text-center">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+          
+          <h3 class="masthead-brand">grapple.ninja</h3>
+          <nav class="nav nav-masthead justify-content-center">
+              <a class="nav-link active" href="https://localhost/gpninja">Home</a>
+              <a class="nav-link" href="https://localhost/gpninja/pricing">Pricing</a>
+              <a class="nav-link" href="https://localhost/gpninja/contact">Contact</a>
+              <a class="nav-link" href="https://localhost/gpninja/account">Login</a>
+          </nav>
+      </div>
+</header>
+
+    <main role="main" class="inner cover">
+        
+    <h1 class="cover-heading">Your Digital Brazilian Jiu Jitsu Training Journal.</h1>
+<p class="lead">Stop trying to track your hard earned mat time manually.&nbsp; Grapple Ninja organizes your Jiu Jitsu&nbsp;Journal and tracks your Mat Time for you so you can level up faster by allowing you to focus on your training goals.</p>
+
+<p class="lead"><a class="btn btn-lg btn-secondary" href="https://grapple.ninja/account">Lets Roll!</a></p>
+
+
+    </main>
+
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p>Cover template</p>
+        </div>
+    </footer>
+</div>
+
+
+<script type="text/javascript">var applicationUrl = "https://localhost/gpninja";</script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script type="text/javascript" src="/gpninja/scripts/site.js"></script>
+
+
+
+
+</body>
+</html>
